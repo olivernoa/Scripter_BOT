de8894bba58016f78b52ae5ceaff001d7605f9f0
olivernoa <92303218+olivernoa@users.noreply.github.com>
2023-08-23 23:44:34 +0600

    main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+import time
+
+def check_website(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            print(f"{url} is up!")
+        else:
+            print(f"{url} is down (Status Code: {response.status_code})")
+    except requests.ConnectionError:
+        print(f"{url} is down!")
+
+if __name__ == "__main__":
+    website_url = "https://scripting.geekybloxyt.repl.co/"
+    interval = 2
+    run_time = 900  # 15 minutes in seconds
+
+    total_time = 0
+    while total_time < run_time:
+        check_website(website_url)
+        time.sleep(interval)
+        total_time += interval
